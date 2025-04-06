b75503af1e66c7321fb43ad4da3ec6c69d274d27
nacchan-kun <sendono.nc17@gmail.com>
2025-04-06 14:05:15 +0300

    fix

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,7 +4,7 @@ import iziToast from 'izitoast';
 // import "izitoast/dist/css/iziToast.min.css";
 
 import errorIcon from '/src/img/izi-toast-svg/error-icon.svg';
-import successIcon from '/img/izi-toast-svg/success-icon.svg';
+import successIcon from '/src/img/izi-toast-svg/success-icon.svg';
 
 const snackbar = {
   formEl: document.querySelector('.form'),
